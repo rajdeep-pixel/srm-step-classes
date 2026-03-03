b90862917406253d38f2332f4a57ee71fcb5ec17
Rajdeep Shaw <rajdeepshaw2006@gmail.com>
2026-03-03 15:57:33 +0530

    Completed

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -1,4 +1,4 @@
-public class UC2 {
+public class OOPsBannerApp {
     public static void main(String[] args) {
         System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "  ***** ");
         System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **      ");
