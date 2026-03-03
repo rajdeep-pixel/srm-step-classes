e85fc2a7cdb73ec8b707ac9eb43e1e9ca1d49a1e
Rajdeep Shaw <rajdeepshaw2006@gmail.com>
2026-03-03 15:59:50 +0530

    Completed

--- OOPsBannerApp.java
+++ OOPsBannerApp.java
@@ -2,7 +2,7 @@
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class UC8 {
+public class OOPsBannerApp {
 
     private static Map<Character, String[]> buildPatternMap() {
         Map<Character, String[]> map = new LinkedHashMap<>();
